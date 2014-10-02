9f91470688662a304a16b4b06821739404cfcae2
Ryan Varick <git@ryanvarick.com>
2014-10-02 16:45:13 +0800

    Import and streamline inline SVG example, with animated fill attribute

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,10 @@ $(document).ready(function() {
 
 	// randomly shift the color?
 	setInterval(function() {
-	    var color = Please.make_color({base_color: "orangered"});
-	    $("h1").css("color", color);
+	    var welcome = Please.make_color({ base_color: "orangered" });
+	    $("h1").css("color", welcome);
+
+		var logo = Please.make_color();
+		$("#svg-test path").css("fill", logo);
 	}, 3000);
 });
