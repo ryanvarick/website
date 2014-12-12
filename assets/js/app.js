0b2f772bc39c0131b90a161db0bb92bb174963aa
Ryan Varick <git@ryanvarick.com>
2014-12-12 14:13:28 -0800

    Early attempt to browersify JS

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+/**
+ ryanvarick.com Stylesheet, v16.0.0
+ - Readable version: https://www.ryanvarick.com/assets/styles/app.less
+ - More info: https://www.ryanvarick.com/colphon
+*/
+
+/*
+<!-- DEVELOPMENT, REMOVE -->
+<script src="vendor/jquery/dist/jquery.js"></script>
+<script src="vendor/pleasejs/Please.js"></script>
+<script src="https://use.typekit.com/ygu4ods.js"></script>
+<script>try { Typekit.load(); } catch(e) { }</script>
+<script type="text/javascript" src="vendor/hashgrid/hashgrid.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    // var grid = new hashgrid();
+});
+</script>
+<script src="//localhost:9091"></script>
+<!-- DEVELOPMENT, REMOVE -->
+*/
+
+// var jquery = require("jquery");
+// var please = require("please");
+// var typekit = require("typekit");
+// var hashgrid = require("hashgrid");
+// var livereload = require("livereload");
+
+require("./typekit.js");
+
+try { Typekit.load(); } catch(e) { }
+console.log("App ready!");
