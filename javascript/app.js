7a1aa2d6a000ca9d4a34c702c9155bdb5fc68d96
Ryan Varick <git@ryanvarick.com>
2015-04-07 11:06:49 -0700

    Disable TypeKit

--- javascript/app.js
+++ javascript/app.js
@@ -18,16 +18,16 @@ $(document).ready(function() {
 // Typekit
 //  - @via https://github.com/wired8/typekit-shim
 //  - Inlined here because filename doesn't match module name
-var typekitId = 'ygu4ods';
-var el = document.createElement('script');
-el.src = '//use.typekit.net/' + typekitId + '.js';
-el.onload = function() {
-    Typekit.load();
-    // delete window.Typekit;
-};
-window.onload = function initTypekit() {
-    document.body.appendChild(el);
-}
+// var typekitId = 'ygu4ods';
+// var el = document.createElement('script');
+// el.src = '//use.typekit.net/' + typekitId + '.js';
+// el.onload = function() {
+//     Typekit.load();
+//     // delete window.Typekit;
+// };
+// window.onload = function initTypekit() {
+//     document.body.appendChild(el);
+// }
 
 console.log("Javascript initialized.");
 
