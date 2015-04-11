eacab23afa8164302c4c56f3976522675c92d059
Ryan Varick <git@ryanvarick.com>
2015-04-11 15:31:47 -0700

    Remove junk

--- javascript/app.js
+++ javascript/app.js
@@ -1,36 +1,4 @@
-/**
- Javascript for ryanvarick.com, v16.0.0 (April, 2015)
-  - Readable version at https://www.ryanvarick.com/site/pub/skins/production/assets/js/app.js
-  - More info at https://github.com/ryanvarick/website
-*/
+/*! Javascript for ryanvarick.com | v16.1.0 (April, 2015) | https://github.com/ryanvarick.com/website */
 
-/*
-<script src="vendor/jquery/dist/jquery.js"></script>
-<script type="text/javascript" src="vendor/hashgrid/hashgrid.js"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-    // var grid = new hashgrid();
-});
-</script>
-<script src="//localhost:9091"></script>
-*/
-
-// Typekit
-//  - @via https://github.com/wired8/typekit-shim
-//  - Inlined here because filename doesn't match module name
-// var typekitId = 'ygu4ods';
-// var el = document.createElement('script');
-// el.src = '//use.typekit.net/' + typekitId + '.js';
-// el.onload = function() {
-//     Typekit.load();
-//     // delete window.Typekit;
-// };
-// window.onload = function initTypekit() {
-//     document.body.appendChild(el);
-// }
-
-console.log("Javascript initialized.");
-
-// Retina.js
-var retina = require('retinajs')
+var retina = require('../vendor/retinajs/dist/retina.js');
 retina.Retina.init(window);
