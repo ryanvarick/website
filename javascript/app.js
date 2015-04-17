584e8ef14c2da7581fa397fb36e1670abc1637df
Ryan Varick <git@ryanvarick.com>
2015-04-17 12:19:28 -0700

    Re-stick footer on orientation change

--- javascript/app.js
+++ javascript/app.js
@@ -3,18 +3,24 @@
 var retina = require('../vendor/retinajs/dist/retina.js');
 retina.Retina.init(window);
 
-
 // look for the portfolio container
 if(document.getElementsByClassName('portfolio-container')[0]) {
 
-    var main = document.getElementsByTagName('main')[0];
-    var mainHeight = main.offsetTop + main.offsetHeight;
-    var windowHeight = window.innerHeight;
+    var stickFooter = function() {
+        var main = document.getElementsByTagName('main')[0];
+        var mainHeight = main.offsetTop + main.offsetHeight;
+        var windowHeight = window.innerHeight;
 
-    // if the window is large enough, stick the footer just above the fold
-    if(windowHeight > mainHeight) {
-        var footer = document.getElementsByTagName('footer')[0];
-        footer.className += ' stick-to-fold';
-        footer.style.top = (windowHeight - 10) + 'px';
+        // if the window is large enough, stick the footer just above the fold
+        if(windowHeight > mainHeight) {
+            var footer = document.getElementsByTagName('footer')[0];
+            footer.className += ' stick-to-fold';
+            footer.style.top = (windowHeight - 10) + 'px';
+        }
     }
+
+    stickFooter();
+
+    // listen for orientation changes on e.g. iPad
+    window.addEventListener('orientationchange', stickFooter);
 }
