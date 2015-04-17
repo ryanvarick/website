a243155011985b2b6f783ec9292aecfad2542cad
Ryan Varick <git@ryanvarick.com>
2015-04-17 14:18:55 -0700

    Dynamically draw portfolio borders

--- javascript/app.js
+++ javascript/app.js
@@ -1,26 +1,87 @@
-/*! Javascript for ryanvarick.com | v16.1.0 (April, 2015) | https://github.com/ryanvarick.com/website */
+/*! Javascript for ryanvarick.com | v16.3.0 (April, 2015) | https://github.com/ryanvarick.com/website */
 
+/**
+ Automatically look for Retina assets
+*/
 var retina = require('../vendor/retinajs/dist/retina.js');
 retina.Retina.init(window);
+
+/**
+ Stick the footer just above the fold
+*/
+var stickFooter = function() {
+
+    // first reset previous class additions
+    var footer = document.getElementsByTagName('footer')[0];
+    footer.className = '';
+
+    var main = document.getElementsByTagName('main')[0];
+    var mainHeight = main.offsetTop + main.offsetHeight;
+    var windowHeight = window.innerHeight;
+
+    // if the window is large enough, stick the footer
+    if(windowHeight > mainHeight) {
+        footer.className += 'stick-to-fold';
+        footer.style.top = (windowHeight - 10) + 'px';
+    }
+};
+
+/**
+ Dynamically draw borders for portfolio elements
+*/
+var drawBorders = function() {
+
+    // get the portfolio items for consideration
+    var items = document.getElementsByClassName('portfolio-container')[0].getElementsByTagName('li');
+
+    // helper function: does the given index have a neighbor to its right?
+    var hasRightNeighbor = function(index) {
+        var elementPos = items[index].offsetLeft;
+
+        // as long as we're not at the last item, check the position of the next item
+        if((index + 1) === items.length) return false;
+        else return (items[index + 1].offsetLeft > elementPos);
+    };
+
+    // helper function: does the given index have a neighbor below it?
+    var hasBottomNeighbor = function(index) {
+        var elementPos = items[index].offsetTop;
+
+        // check all the remaining items for a bottom neighbor
+        var bottomNeighborFound = false;
+        for(var i = index; i < items.length; i++) {
+            bottomNeighborFound = (items[i].offsetTop > elementPos);
+        }
+        return bottomNeighborFound;
+    };
+
+    // loop through the items and draw borders as necessary
+    for(var i = 0; i < items.length; i++) {
+
+        // first reset previous border values (e.g. after orientation change)
+        items[i].style.borderWidth = 0;
+
+        // now check if it should have a right and/or bottom border
+        if(hasRightNeighbor(i)) {
+            items[i].style.borderRightWidth = '1px';
+        }
+        if(hasBottomNeighbor(i)) {
+            items[i].style.borderBottomWidth = '1px';
+        }
+    } // end for
+
+};
 
 // look for the portfolio container
 if(document.getElementsByClassName('portfolio-container')[0]) {
 
-    var stickFooter = function() {
-        var main = document.getElementsByTagName('main')[0];
-        var mainHeight = main.offsetTop + main.offsetHeight;
-        var windowHeight = window.innerHeight;
+    // draw borders and then listen for events that might cause flexbox wrapping
+    window.addEventListener('orientationchange', drawBorders);
+    window.addEventListener('resize', drawBorders);
+    drawBorders();
 
-        // if the window is large enough, stick the footer just above the fold
-        if(windowHeight > mainHeight) {
-            var footer = document.getElementsByTagName('footer')[0];
-            footer.className += ' stick-to-fold';
-            footer.style.top = (windowHeight - 10) + 'px';
-        }
-    }
-
-    stickFooter();
-
-    // listen for orientation changes on e.g. iPad
+    // stick the footer and then listen for orientation changes on e.g. iPad
     window.addEventListener('orientationchange', stickFooter);
+    window.addEventListener('resize', stickFooter);
+    stickFooter();
 }
