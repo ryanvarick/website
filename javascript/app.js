bfeb600ac10f8d25648539fe503200f189b591fe
Ryan Varick <git@ryanvarick.com>
2015-04-17 10:30:19 -0700

    Improve sticky footer

--- javascript/app.js
+++ javascript/app.js
@@ -2,3 +2,19 @@
 
 var retina = require('../vendor/retinajs/dist/retina.js');
 retina.Retina.init(window);
+
+
+// look for the portfolio container
+if(document.getElementsByClassName('portfolio-container')[0]) {
+
+    // if found, measure the window
+    var minHeight = 800;
+    var windowHeight = window.innerHeight;
+
+    // if the window is large enough, stick the footer just above the fold
+    if(windowHeight > minHeight) {
+        var footer = document.getElementsByTagName('footer')[0];
+        footer.className += ' stick-to-fold';
+        footer.style.top = (windowHeight - 10) + 'px';
+    }
+}
