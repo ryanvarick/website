c2f7c2919a9084faafda69356e5e835818e2c7c8
Ryan Varick <git@ryanvarick.com>
2015-04-17 10:49:13 -0700

    Better iPad support

--- javascript/app.js
+++ javascript/app.js
@@ -7,12 +7,12 @@ retina.Retina.init(window);
 // look for the portfolio container
 if(document.getElementsByClassName('portfolio-container')[0]) {
 
-    // if found, measure the window
-    var minHeight = 800;
+    var main = document.getElementsByTagName('main')[0];
+    var mainHeight = main.offsetTop + main.offsetHeight;
     var windowHeight = window.innerHeight;
 
     // if the window is large enough, stick the footer just above the fold
-    if(windowHeight > minHeight) {
+    if(windowHeight > mainHeight) {
         var footer = document.getElementsByTagName('footer')[0];
         footer.className += ' stick-to-fold';
         footer.style.top = (windowHeight - 10) + 'px';
